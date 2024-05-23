e885067abce36298554d3c91366cf057d3bdcb78
Yongmin Hong <revi@omglol.email>
2024-05-23 13:17:47 +0900

    translation: init
    Summary:
    Ref T151. Work TBD.
    
    Signed-off-by: Yongmin Hong <revi@omglol.email>
    
    Test Plan: `npm run build` & `npm run serve`
    
    Reviewers: O2 Coders, O1 revi & automations, revi
    
    Reviewed By: O2 Coders, O1 revi & automations, revi
    
    Maniphest Tasks: T151
    
    Differential Revision: https://issuetracker.revi.xyz/D127

---
+++ translationinit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright (C) 2024 Hong Yongmin <https://revi.xyz>
+# SPDX-License-Identifier : Apache-2.0
+
+echo 'config: do not overwrite existing pages'
+
+echo 'now copying blog.'
+cp -r -n blog/** i18n/ko/
