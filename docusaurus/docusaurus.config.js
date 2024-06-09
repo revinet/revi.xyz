59cb469131f5f7a702749deac3b1a362a3eb170d
Yongmin Hong <revi@omglol.email>
2024-06-09 20:20:57 +0900

    docusaurus(config): use image for cc license link
    Summary:
    For fun :tm:, and some sort of sync with mediawiki. Also modify some `npm run` rules.
    
    Signed-off-by: Yongmin Hong <revi@omglol.email>
    
    Test Plan: `npm run dbuild`
    
    Reviewers: O2 Coders, O1 revi & automations, revi
    
    Reviewed By: O2 Coders, O1 revi & automations, revi
    
    Differential Revision: https://issuetracker.revi.xyz/D220

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -247,8 +247,11 @@ const config = {
                 to: '/privacy-policy',
               },
               {
-                label: 'License CC BY ND 2.0 KR',
-                href: 'https://creativecommons.org/licenses/by-nd/2.0/kr/',
+                html: `
+                      <a href="https://creativecommons.org/licenses/by-nd/2.0/kr/", target="_blank" rel="noreferrer" alt="CC BY ND 2.0 KR License">
+                        <img src="/img/by-nd.svg" alt="CC BY ND 2.0 KR License" width="88" height="31" />
+                      </a>
+                    `,
               },
             ],
           },
