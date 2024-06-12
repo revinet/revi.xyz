d6732340f44353942a4f7c9f9a4697cc7317fac6
Yongmin Hong <revi@omglol.email>
2024-06-12 17:09:16 +0900

    CI: renovate timezone, sync Cloudflare Pages invocation requirement
    Summary:
    Set renovate tz to Asia/Seoul.
    Also:
     - sync Cloudflare Pages invocationr requirement with CI
     - set all-in-one lint script.
     - set `--fund=false` to suppress funding notices
     - set `--omit=optional` to make non-renovate script skip `optionalDependency`
    
    Signed-off-by: Yongmin Hong <revi@omglol.email>
    
    Test Plan: `npm run lint`.
    
    Reviewers: O2 Coders, O1 revi & automations, revi
    
    Reviewed By: O2 Coders, O1 revi & automations, revi
    
    Differential Revision: https://issuetracker.revi.xyz/D281

---
+++ .github/lint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright 2024 Hong Yongmin
+# SPDX-License-Identifier: Apache-2.0
+# Runs all the lint scripts
+
+# Continue on error
+set +e
+
+echo 'Starting the linting process...'
+echo 'Running renovate-config-validator now...'
+npx renovate-config-validator
+
+echo 'Checking if arc is installed...'
+if command -v arc &> /dev/null; then
+  echo 'arc is installed. Running arc lint now...'
+  arc lint
+else
+  echo 'arc is not installed, skipping...'
+fi
+
+echo 'Running prettier check now...'
+npx prettier --check .
+
+echo 'Running eslint check now...'
+cd docusaurus
+npx eslint .
+echo 'Linting process completed successfully!'
+exit 0
